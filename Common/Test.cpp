69b24f3642d92a75e490cfe3ed3fdd6e25e4f903
woc <woc@woc.com>
2020-12-30 17:18:11 +0800

    增加一个导出函数demo

---
+++ Common/Test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "Test.h"
+
+void Test::ExternFunc()
+{
+	return;
+}
---
+++ Common/Test.h
@@ -0,0 +1,6 @@
+#pragma once
+extern "C" class Test
+{
+	__declspec(dllexport) void ExternFunc();
+};
+
